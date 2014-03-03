46b1862773506c1c8c7ccdff4cd28b6de9ac7bce
harun.al.rosyid <spinfusor.gt@gmail.com>
2014-03-03 20:08:35 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Railliar::Application.config.secret_key_base = 'efecbb6c9ec98dc965aea5794fd883f5da8a648be036fd46411b61f7ec0d66e77249a30f9cde6aed6e559ad19135877f2da7ff987770a1dcc62c7450eb09f8f5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Railliar::Application.config.session_store :cookie_store, key: '_railliar_session'
